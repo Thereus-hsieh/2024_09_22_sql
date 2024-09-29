a81900c1daa0d5c20ba0c4854a3d4fb93f8cd6df
THEREUS <ZX26705089@gmail.com>
2024-09-29 16:11:05 +0800

    weather

--- sql/LESSON2_5.sql
+++ sql/LESSON2_5.sql
@@ -1,11 +1,24 @@
 DROP TABLE IF EXISTS hospital;
 
 CREATE TABLE IF NOT EXISTS hospital (
-    "serial number" SERIAL PRIMARY KEY,
-    "B" VARCHAR(5) NOT NULL,
-	"C" VARCHAR(30) NOT NULL UNIQUE,
-	"D" VARCHAR(15),
-	"E" VARCHAR(7),
-	"F" VARCHAR(7),
-	"G" VARCHAR(50)
+    "ID" SERIAL PRIMARY KEY,
+    "STATE" VARCHAR(5) NOT NULL,
+	"NANE" VARCHAR(30) NOT NULL UNIQUE,
+	"PHONE" VARCHAR(15),
+	"EXT" VARCHAR(7),
+	"CONTACT" VARCHAR(7),
+	"ADDRESS" VARCHAR(50)
     );
+	
+/*城市,啟始時間,結束時間,最高溫度,最低溫度,感覺*/
+
+CREATE TABLE IF NOT EXISTS weather(
+    id SERIAL PRIMARY KEY,
+	city VARCHAR(20) NOT NULL,
+    startDate timestamp,
+	endDate timestamp,
+	hight real,
+	low real,
+	status VARCHAR(20)
+);
+
