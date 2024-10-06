8209da172d671eed3052d59be7a7239724b14e64
THEREUS <ZX26705089@gmail.com>
2024-10-06 13:55:07 +0800

    zip

--- sql/lesson3_3.sql
+++ sql/lesson3_3.sql
@@ -15,3 +15,15 @@ WHERE name LIKE '台%';
 SELECT id as 序號,code as 代碼, name as 車站名稱, ename as 英文名稱
 FROM stations2
 WHERE name LIKE '%港';
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, ename as 英文名稱
+FROM stations2
+WHERE id <=30 and id >=10;
+
+SELECT code as 代碼 ,name as 車站中文名稱,ename as 車站英文名稱
+from stations2
+WHERE ename LIKE 'Ta%';
+
+
+
+
