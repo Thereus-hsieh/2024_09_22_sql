446d60dd1561e30cf71d95246a7fbb6ff3a928f3
THEREUS <ZX26705089@gmail.com>
2024-11-03 15:06:40 +0800

    env不上傳

---
+++ sql/leson7.sql
@@ -0,0 +1,21 @@
+CREATE TABLE IF NOT EXISTS 市場1(
+	name VARCHAR(20),
+	country VARCHAR(20),
+	PRIMARY KEY(name)
+);
+
+CREATE TABLE IF NOT EXISTS 股市1(
+	stock_id SERIAL,
+	date DATE,
+	open NUMERIC(17,10),
+	high NUMERIC(17,10),
+	low NUMERIC(17,10),
+	close NUMERIC(17,10),
+	adj_close NUMERIC(17,10),
+	volume BIGINT DEFAULT 0,
+	name VARCHAR(20),
+	PRIMARY KEY(stock_id),
+	FOREIGN KEY(name) REFERENCES 市場1(name) 
+	ON DELETE NO ACTION
+	ON UPDATE CASCADE
+);
